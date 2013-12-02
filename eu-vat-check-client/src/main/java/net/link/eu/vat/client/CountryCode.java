400f891d392d05e9fbf412395d509c400e296e05
Gianni Van Hoecke <gianni.vanhoecke@lin-k.net>
2013-12-02 10:05:42 +0100

    All country codes implemented

--- eu-vat-check-client/src/main/java/net/link/eu/vat/client/CountryCode.java
+++ eu-vat-check-client/src/main/java/net/link/eu/vat/client/CountryCode.java
@@ -10,7 +10,170 @@ package net.link.eu.vat.client;
 public enum CountryCode {
 
     /**
-     * Belgium
+     * Austria
+     * (Unavailable almost daily around 06:00 AM for a few minutes)
      */
-    BE
+    AT,
+
+    /**
+     * Belgium
+     * (Available 24 hours/day, 7 days/week)
+     */
+    BE,
+
+    /**
+     * Bulgaria
+     * (Unknown)
+     */
+    BG,
+
+    /**
+     * Cyprus
+     * (Available 24 hours/day, 7 days/week)
+     */
+    CY,
+
+    /**
+     * Czech Republic
+     * (Unavailable everyday around 07:00 AM for about 20 minutes)
+     */
+    CZ,
+
+    /**
+     * Germany
+     * (Available from 05:00 AM to 11:00 PM)
+     */
+    DE,
+
+    /**
+     * Denmark
+     * (Unavailable every day between 04:00 AM and 05:00 AM; Every Tuesday from 06:45 AM to 07:15 AM)
+     */
+    DK,
+
+    /**
+     * Estonia
+     * (Available 24 hours/day, 7 days/week)
+     */
+    EE,
+
+    /**
+     * Greece
+     * (Available 24 hours/day, 7 days/week)
+     */
+    EL,
+
+    /**
+     * Spain
+     * (Unavailable daily around 11:00 PM for a few minutes)
+     */
+    ES,
+
+    /**
+     * Finland
+     * (Unavailable every Sunday between 05:40 AM and 05:50 AM)
+     */
+    FI,
+
+    /**
+     * France
+     * (Unavailable almost everyday between 01:30 AM and 01:40 AM)
+     */
+    FR,
+
+    /**
+     * United Kingdom
+     * (Unavailable every Saturday from 07:30 AM to 10:30 AM and almost daily from around 04:30 AM to 04:40 AM)
+     */
+    GB,
+
+    /**
+     * Croatia
+     * (Unknown)
+     */
+    HR,
+
+    /**
+     * Hungary
+     * (Available 24 hours/day, 7 days/week)
+     */
+    HU,
+
+    /**
+     * Ireland
+     * (Unavailable on Sunday nights for maximum 2 hours)
+     */
+    IE,
+
+    /**
+     * Italy
+     * (Unavailable every Monday to Saturday from 08:00 PM for 30 to 60 minutes)
+     */
+    IT,
+
+    /**
+     * Lithuania
+     * (Available 24 hours/day, 7 days/week)
+     */
+    LT,
+
+    /**
+     * Luxembourg
+     * (Available 24 hours/day, 7 days/week)
+     */
+    LU,
+
+    /**
+     * Latvia
+     * (Available 24 hours/day, 7 days/week)
+     */
+    LV,
+
+    /**
+     * Malta
+     * (Unavailable every Thursday from 07:00 AM to 07:30 AM)
+     */
+    MT,
+
+    /**
+     * The Netherlands
+     * (Unavailable every weekend from Saturday 09:50 PM to Sunday 09:40 PM)
+     */
+    NL,
+
+    /**
+     * Poland
+     * (Available 24 hours/day, 7 days/week)
+     */
+    PL,
+
+    /**
+     * Portugal
+     * (Unavailable every Friday from around 23:30 for about 30 minutes or more)
+     */
+    PT,
+
+    /**
+     * Romania
+     * (Unavailable almost every weekend from Saturday 09:50 PM to Sunday 09:50 PM)
+     */
+    RO,
+
+    /**
+     * Sweden
+     * (Available 24 hours/day, 7 days/week)
+     */
+    SE,
+
+    /**
+     * Slovenia
+     * (Unknown)
+     */
+    SI,
+
+    /**
+     * Slovakia
+     * (Available 24 hours/day, 7 days/week)
+     */
+    SK
 }
